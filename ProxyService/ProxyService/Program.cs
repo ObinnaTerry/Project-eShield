ae606cf8c9a57e1d1dcbf785899efe9a930608e9
ObinnaTerry <terrydona90@gmail.com>
2023-07-01 16:38:42 +0200

    Feat: Adds New Project
    Tthis is the setup of the proxy server

---
+++ ProxyService/ProxyService/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProxyService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
