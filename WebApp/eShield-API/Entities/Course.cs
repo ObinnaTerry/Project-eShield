9e2fa8dd2c627485bff9229413e521163432bd44
ObinnaTerry <terrydona90@gmail.com>
2023-07-13 16:23:17 +0200

    Feat: Adds Database Entity Models

---
+++ WebApp/eShield-API/Entities/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace eShield_API.Entities;
+
+public partial class Course
+{
+    public int Id { get; set; }
+
+    public string CourseName { get; set; } = null!;
+
+    public int ProfessorId { get; set; }
+}
---
+++ WebApp/eShield-API/Entities/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace eShield_API.Entities;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public virtual ICollection<ExamStudent> ExamStudents { get; set; } = new List<ExamStudent>();
+
+    public virtual ICollection<VisitedSite> VisitedSites { get; set; } = new List<VisitedSite>();
+}
