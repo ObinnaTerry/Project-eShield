d6fccfa3ab18adde0d8558db0a813655f2290dfa
sanjana <66816050+Sanjana1202@users.noreply.github.com>
2023-07-15 16:18:18 +0200

    Refactor: Updates Professor Table by Adding Column Id referencing to Course Id

--- WebApp/eshield database/dbo/Tables/Professor.sql
+++ WebApp/eshield database/dbo/Tables/Professor.sql
@@ -3,5 +3,7 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [FirstName] VARCHAR(50) NOT NULL, 
     [LastName] VARCHAR(50) NOT NULL, 
-    [Email] VARCHAR(50) NOT NULL
+    [Email] VARCHAR(50) NOT NULL,
+    [CourseId] INT NOT NULL,
+    CONSTRAINT FK_Professor_Course FOREIGN KEY ([CourseId]) REFERENCES [dbo].[Course]([Id])
 )
