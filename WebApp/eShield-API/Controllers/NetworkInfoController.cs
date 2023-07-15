e4b6f9ddf2a3547eeceb791fcc368ab75f8f6bec
ObinnaTerry <terrydona90@gmail.com>
2023-07-15 17:49:52 +0200

    Feat: Adds Feat to Create Student Network Info

---
+++ WebApp/eShield-API/Controllers/NetworkInfoController.cs
@@ -0,0 +1,35 @@
+﻿using eShield_API.DataService;
+using eShield_API.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace eShield_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NetworkInfoController : ControllerBase
+    {
+        private readonly NetworkInfoDataService _networkInfoDataService;
+
+        public NetworkInfoController(NetworkInfoDataService networkInfoDataService)
+        {
+            _networkInfoDataService = networkInfoDataService;
+        }
+
+        // POST api/<NetworkInfoController>
+        [HttpPost]
+        public IActionResult Post([FromBody] NetworkInfoDTO networkInfoDTO)
+        {
+            bool? result = _networkInfoDataService.Create(networkInfoDTO);
+
+            if(result == null)
+            {
+                return BadRequest(400);
+            }
+
+            return Ok();
+        }
+
+    }
+}
