00007c5fd68374929d43774541e042d49dfaa26f
ObinnaTerry <terrydona90@gmail.com>
2023-07-15 13:14:26 +0200

    Feat: Adds Create Exam Functionality
    This change implements the complete CRUD methods for exam along with relevant services

---
+++ WebApp/eShield-API/DTOs/ExamDTO.cs
@@ -0,0 +1,28 @@
+﻿namespace eShield_API.DTOs
+{
+    public class ExamDTO
+    {
+        private int _id;
+
+        public ExamDTO(int createdBy, int courseId, DateTime examDate, TimeSpan startTime, TimeSpan endTime)
+        {
+            CreatedBy = createdBy;
+            CourseId = courseId;
+            ExamDate = examDate;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+        public int Id { get => _id; set => _id = value; }
+
+        public int CreatedBy { get; set; }
+
+        public int CourseId { get; set; }
+
+        public DateTime ExamDate { get; set; }
+
+        public TimeSpan StartTime { get; set; }
+
+        public TimeSpan EndTime { get; set; }
+    }
+}
