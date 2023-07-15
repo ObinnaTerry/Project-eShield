cc0cf636314c9959e6c3de3987dfc6e92bac3a25
sanjana <66816050+Sanjana1202@users.noreply.github.com>
2023-07-15 18:16:10 +0200

    Feat: Adds CourseDTO, implements CourseController & DataService

---
+++ WebApp/eShield-API/DTOs/CourseDTO.cs
@@ -0,0 +1,30 @@
+﻿using eShield.CoreData.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace eShield_API.DTOs
+{
+    public class CourseDTO
+    {
+
+        public int _id;
+
+        public string _courseName;
+
+        public int _professorId;
+
+        public CourseDTO(int id, string courseName, int professorId)
+        {
+            _id = id;
+            _courseName = courseName;
+            _professorId = professorId;
+        }
+        public int Id { get; set; }
+
+        public string CourseName { get; set; }
+
+        public int ProfessorId { get; set; }
+
+    }
+}
+
+
---
+++ WebApp/eShield-API/DataService/CourseDataService.cs
@@ -0,0 +1,49 @@
+﻿using eShield.CoreData.Entities;
+using eShield.CoreData.Interfaces;
+using eShield_API.DTOs;
+
+namespace eShield_API.DataService
+{
+    public class CourseDataService
+    {
+        private readonly ICourseRepo _courseRepo;
+
+        public CourseDataService(ICourseRepo courseRepo)
+        {
+            _courseRepo = courseRepo;
+           
+        }
+
+        public Course Create(CourseDTO courseDTO)
+        {
+            Course course = new Course()
+            {
+                CourseName = courseDTO.CourseName,
+                ProfessorId = courseDTO.ProfessorId
+                
+            };
+
+            _courseRepo.Insert(course);
+            _courseRepo.Save();
+
+            return course;
+        }
+
+          public CourseDTO  ReadByProfId(int profid)
+        {
+            Course? course = _courseRepo.GetAll().Where(x => x.ProfessorId == profid).FirstOrDefault();
+
+            if (course == null)
+            {
+                return null;
+            }
+
+            CourseDTO courseDTO = new CourseDTO(course.Id, course.CourseName, course.ProfessorId);
+
+            return courseDTO;
+        }
+
+     
+    }
+}
+
