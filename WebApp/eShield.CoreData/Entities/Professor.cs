4e1de04793708c9c47863522aa8f3e90abb33315
sanjana <66816050+Sanjana1202@users.noreply.github.com>
2023-07-15 13:23:06 +0200

    Refactor: Update Professor class with Course navigation property

--- WebApp/eShield.CoreData/Entities/Professor.cs
+++ WebApp/eShield.CoreData/Entities/Professor.cs
@@ -12,4 +12,6 @@ public partial class Professor
     public string LastName { get; set; } = null!;
 
     public string Email { get; set; } = null!;
+
+    public virtual Course Course { get; set; }
 }
