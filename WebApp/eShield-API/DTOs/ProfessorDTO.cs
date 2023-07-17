aa3f93e55dbb602ae7d29c0448d8c8fa74bb20d7
ObinnaTerry <terrydona90@gmail.com>
2023-07-17 16:27:29 +0200

    Refactor: Removes Circular Relationship Between Professor and Course Tables

---
+++ WebApp/eShield-API/DTOs/ProfessorDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace eShield_API.DTOs
+{
+    public class ProfessorDTO
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public int CourseId { get; set; }
+    }
+}
--- WebApp/eshield database/dbo/Tables/Course.sql
+++ WebApp/eshield database/dbo/Tables/Course.sql
@@ -2,5 +2,4 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [CourseName] VARCHAR(50) NOT NULL, 
-    [ProfessorId] INT NOT NULL
 )
